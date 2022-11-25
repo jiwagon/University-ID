e4a2f0cb81c4c91babd2a92ad0623a2a69670886
jiwagon <jiwagon11@gmail.com>
2022-11-25 14:09:51 -0500

    functional email entry

--- app/src/main/java/edu/psu/ist/hcdd340/pennstateid/LoginActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/pennstateid/LoginActivity.java
@@ -2,11 +2,15 @@ package edu.psu.ist.hcdd340.pennstateid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final static String TAG = "LOG_IN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         button.setOnClickListener(this);
     }
 
+    /**
+     * Retrieve user input from EditText
+     */
     @Override
     public void onClick(View view) {
-
+        // Select the UI Element and getText() for email input
+        EditText emailEditText = findViewById(R.id.editText_email);
+        String email = emailEditText.getText().toString();
+        // Log the text
+        Log.d(TAG, "Email entered: " + email);
     }
 }
